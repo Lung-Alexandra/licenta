c30201100e8df9af384f9816e5f6759bb2ad7166
Katherine456719 <alexandra.lung@s.unibuc.ro>
2024-07-01 11:57:20 +0300

    Fix

--- src/page_discard.c
+++ src/page_discard.c
@@ -1,9 +1,7 @@
 #include "page_discard.h"
 
 void discard_empty_page(void *page_address, int page_size) {
-//    int result = madvise(page_address, page_size, MADV_FREE);
-    int result = munmap(page_address, page_size);
-
+    int result = madvise(page_address, page_size, MADV_FREE);
     if (result == -1) {
         printf("Error\n");
         exit(EXIT_FAILURE);
